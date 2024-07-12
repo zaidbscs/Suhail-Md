519d6edf740c5a4d699ff2dcadc8918e581d293d
zaidbscs <137680240+zaidbscs@users.noreply.github.com>
2024-07-12 10:02:50 +0500

    Update config.js

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ global.SESSION_ID = process.env.SESSION_ID ||  "SUHAIL_00_39_06_19_ewogICJjcmVkc
 
 module.exports = {
 
-  menu: process.env.MENU || "3", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Suhail-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
+  menu: process.env.MENU || "", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Suhail-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
 
   HANDLERS: process.env.PREFIX  || ".",
   BRANCH  : process.env.BRANCH  || "main",
